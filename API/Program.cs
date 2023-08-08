6ffbc4f72539eae3c03ce7242176796b349a2e71
3BoOoD97 <abodn70@hotmail.com>
2023-08-08 17:11:44 +0200

    Adding a DbContext class

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,13 @@
+using API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddDbContext<DataContext>(options =>
+    options.Sqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
